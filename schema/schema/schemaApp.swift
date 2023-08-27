89d0e2d4bdf473467c057098a6f2961e001c55b5
jesus <jecastellano@eservicios.indra.es>
2023-08-27 13:24:24 +0200

    Add Schema configs.

---
+++ schema/schema/schemaApp.swift
@@ -0,0 +1,17 @@
+//
+//  schemaApp.swift
+//  schema
+//
+//  Created by Jesu Castellano on 27/8/23.
+//
+
+import SwiftUI
+
+@main
+struct schemaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
